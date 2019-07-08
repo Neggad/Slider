3ea8ca97fdadbe28c4868fe59d2b0cbcce586ef4
Johan Dagvall <johan.dagvall@gmail.com>
2019-07-08 20:24:33 +0200

    some sort of slider functionality, but not what I want, they have fixed positions :(

--- slider.js
+++ slider.js
@@ -1,25 +1,44 @@
-var display = [0, 1, 2]
+var currentSliderIds = [0, 1, 2];
+var allSliders = [];
 //https://freefrontend.com/css-carousels/
-var numberOfPersons = 5;
+var numberOfPersons = 0;
 const generateUsers = () => {
   //call API and create slides and buttons after
   getUsers
     .then(function (data) {
       createArrowBtn('left');
       console.log("something", data.results)
+      numberOfPersons = data.results.length;
       data.results.map((person, index) => {
-        createSlide(person);
+        createSlide(person, index);
         createButton(index);
       })
       createArrowBtn('right');
     })
+    .then(function(){
+      
+    })
 }
-const createSlide = (person) => {
+const createSlide = (person, index) => {
   //Parent container for slides
   let currentdiv = document.getElementById('sliderContainer');
   //create div for person
   let newDiv = document.createElement("div");
+  newDiv.id = "slider_"+index;
+  allSliders.push(index);
   newDiv.className = 'slide';
+  if(index > 2){
+    newDiv.className += ' invisible';
+  }
+
+//test
+let testDiv = document.createElement("div");
+
+let testContent = document.getElementById("test");
+testDiv.append(person.name.first + " " + person.name.last)
+testDiv.className="test"
+testContent.appendChild(testDiv);
+
   //set content of said div
   let newContent = document.createTextNode(person.name.first + " " + person.name.last);
   newDiv.appendChild(newContent);
@@ -34,10 +53,7 @@ const createButton = (index) => {
   newButton.className = 'button';
   let newButtonContent = document.createTextNode(index);
   newButton.appendChild(newButtonContent);
-
-  // nextButton.after(newButton);
   buttonContainer.appendChild(newButton);
-
 }
 
 const createArrowBtn = (dir) => {
@@ -55,20 +71,50 @@ const createArrowBtn = (dir) => {
   buttonContainer.appendChild(dirButton);
 }
 
-const goTo = (dir) => {
-  console.log("display before:", display)
-  if (dir === 'left') {
-    for (let i = 0; i < display.length; i++) {
-      if (display[i] === 0) {
-
-        display[i] = numberOfPersons;
+const toggleSliders = (sliders) =>{
+  console.log("SLIDERS", sliders)
+  for(let i = 0; i < allSliders.length; i++){
+    let currentSlider = document.getElementById("slider_"+allSliders[i]);
+    console.log("(all)slider:", allSliders[i])
+    // console.log("sliders.contains(allSliders[i])", sliders.contains(allSliders[i]))
+    if(sliders.includes(allSliders[i])){
+      console.log("sliders includes current number:", allSliders[i])
+      if(currentSlider.classList.contains("invisible")){
+        console.log("show current slider:", "slider_"+allSliders[i])
+        currentSlider.classList.remove("invisible")
       }
-      else {
-        display[i] = display[i] - 1;
+    }
+    else{
+      if(!currentSlider.classList.contains("invisible")){
+        currentSlider.classList.add("invisible")
       }
     }
   }
-  console.log("display:", display)
+}
+const goTo = (dir) => {
+  if (dir === 'left') {
+    for (let i = 0; i < currentSliderIds.length; i++) {
+      if (currentSliderIds[i] === 0) {
+        currentSliderIds[i] = numberOfPersons-1;
+      }
+      else {
+        currentSliderIds[i] = currentSliderIds[i] - 1;
+      }
+    }
+  }
+  else{
+    for (let i = 0; i < currentSliderIds.length; i++) {
+      if (currentSliderIds[i] === numberOfPersons-1) {
+        currentSliderIds[i] = 0;
+      }
+      else {
+        currentSliderIds[i] = currentSliderIds[i] + 1;
+      }
+    }
+  }
+
+  toggleSliders(currentSliderIds);
+  console.log("currentSliderIds:", currentSliderIds)
   console.log("go to:", dir)
 }
 

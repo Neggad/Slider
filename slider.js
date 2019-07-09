ee319f8624558f4cc2771b9c1153f31ae82c22a4
Dagvall Johan <Johan.Dagvall@etteplan.com>
2019-07-09 10:11:45 +0200

    commit before branch, changed the incrementation to be handled in an object

--- slider.js
+++ slider.js
@@ -1,4 +1,5 @@
 var currentSliderIds = [0, 1, 2];
+var sliderIdAndPos = [];
 var allSliders = [];
 //https://freefrontend.com/css-carousels/
 var numberOfPersons = 0;
@@ -15,8 +16,8 @@ const generateUsers = () => {
       })
       createArrowBtn('right');
     })
-    .then(function(){
-      
+    .then(function () {
+
     })
 }
 const createSlide = (person, index) => {
@@ -24,26 +25,28 @@ const createSlide = (person, index) => {
   let currentdiv = document.getElementById('sliderContainer');
   //create div for person
   let newDiv = document.createElement("div");
-  newDiv.id = "slider_"+index;
+  newDiv.id = "slider_" + index;
   allSliders.push(index);
   newDiv.className = 'slide';
-  if(index > 2){
+  if (index > 2) {
     newDiv.className += ' invisible';
   }
 
-//test
-let testDiv = document.createElement("div");
+  //test
+  let testDiv = document.createElement("div");
 
-let testContent = document.getElementById("test");
-testDiv.append(person.name.first + " " + person.name.last)
-testDiv.className="test"
-testContent.appendChild(testDiv);
+  let testContent = document.getElementById("test");
+  testDiv.append(person.name.first + " " + person.name.last)
+  testDiv.className = "test"
+  testContent.appendChild(testDiv);
 
   //set content of said div
   let newContent = document.createTextNode(person.name.first + " " + person.name.last);
   newDiv.appendChild(newContent);
 
   currentdiv.append(newDiv);
+
+  sliderIdAndPos.push({ pos: index, id: index })
 }
 const createButton = (index) => {
   //Parent container for buttons
@@ -71,21 +74,21 @@ const createArrowBtn = (dir) => {
   buttonContainer.appendChild(dirButton);
 }
 
-const toggleSliders = (sliders) =>{
+const toggleSliders = (sliders) => {
   console.log("SLIDERS", sliders)
-  for(let i = 0; i < allSliders.length; i++){
-    let currentSlider = document.getElementById("slider_"+allSliders[i]);
-    console.log("(all)slider:", allSliders[i])
+  for (let i = 0; i < sliders.length; i++) {
+    let currentSlider = document.getElementById("slider_" + sliders[i].id);
+    console.log("slider at i:", sliders[i])
     // console.log("sliders.contains(allSliders[i])", sliders.contains(allSliders[i]))
-    if(sliders.includes(allSliders[i])){
-      console.log("sliders includes current number:", allSliders[i])
-      if(currentSlider.classList.contains("invisible")){
-        console.log("show current slider:", "slider_"+allSliders[i])
+    if (sliders[i].pos < 3) {
+      console.log("slider is inside of visible area:", sliders[i])
+      if (currentSlider.classList.contains("invisible")) {
+        console.log("show current slider:", "slider_" + sliders[i])
         currentSlider.classList.remove("invisible")
       }
     }
-    else{
-      if(!currentSlider.classList.contains("invisible")){
+    else {
+      if (!currentSlider.classList.contains("invisible")) {
         currentSlider.classList.add("invisible")
       }
     }
@@ -93,28 +96,44 @@ const toggleSliders = (sliders) =>{
 }
 const goTo = (dir) => {
   if (dir === 'left') {
-    for (let i = 0; i < currentSliderIds.length; i++) {
-      if (currentSliderIds[i] === 0) {
-        currentSliderIds[i] = numberOfPersons-1;
+    for (let i = 0; i < sliderIdAndPos.length; i++) {
+      if (sliderIdAndPos[i].pos === 0) {
+        sliderIdAndPos[i].pos = numberOfPersons - 1;
       }
       else {
-        currentSliderIds[i] = currentSliderIds[i] - 1;
+        sliderIdAndPos[i].pos = sliderIdAndPos[i].pos - 1;
       }
     }
+    // for (let i = 0; i < currentSliderIds.length; i++) {
+    //   if (currentSliderIds[i] === 0) {
+    //     currentSliderIds[i] = numberOfPersons-1;
+    //   }
+    //   else {
+    //     currentSliderIds[i] = currentSliderIds[i] - 1;
+    //   }
+    // }
   }
-  else{
-    for (let i = 0; i < currentSliderIds.length; i++) {
-      if (currentSliderIds[i] === numberOfPersons-1) {
-        currentSliderIds[i] = 0;
+  else {
+    for (let i = 0; i < sliderIdAndPos.length; i++) {
+      if (sliderIdAndPos[i].pos === numberOfPersons - 1) {
+        sliderIdAndPos[i].pos = 0;
       }
       else {
-        currentSliderIds[i] = currentSliderIds[i] + 1;
+        sliderIdAndPos[i].pos = sliderIdAndPos[i].pos + 1;
       }
     }
+    // for (let i = 0; i < currentSliderIds.length; i++) {
+    //   if (currentSliderIds[i] === numberOfPersons-1) {
+    //     currentSliderIds[i] = 0;
+    //   }
+    //   else {
+    //     currentSliderIds[i] = currentSliderIds[i] + 1;
+    //   }
+    // }
   }
 
-  toggleSliders(currentSliderIds);
-  console.log("currentSliderIds:", currentSliderIds)
+  toggleSliders(sliderIdAndPos);
+  console.log("sliderIdAndPos:", sliderIdAndPos)
   console.log("go to:", dir)
 }
 
